2173743ce271f683a9ab2d73ba7d686232575f82
Whcreating <llollipot@gmail.com>
2026-06-05 10:45:33 +0500

    Подготовка пространства flet

--- src/main.py
+++ src/main.py
@@ -1,25 +1,12 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+async def main(page: ft.Page):
+    page.theme_mode = ft.ThemeMode.DARK
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
+    page.title = "Handwriting Dec."
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
-    )
+
 
 
 ft.run(main)
